d698cd5aa427f5c3cf04628655b32bb58275dc54
Tucker Self <tself3@ut.utm.edu>
2024-10-03 15:00:04 -0500

    Fixed Bubble

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -33,13 +33,16 @@ void bubbleSortB(vector<int> &ar){
 }
 
 void bubbleSortC(vector<int> &ar){
+    bool swapped = 1;
     int length = ar.size();
-    for(int i = length; i > 1; i--){
-        for(int j = length; j > i+1; j--){
-            if(ar[j] < ar[j-1]){
-                int temp = ar[j];
-                ar[j] = ar[j-1];
-                ar[j-1] = temp;
+    while(swapped){
+        swapped = 0;
+        for(int i = length-1; i >= 0; i--){
+            if(ar[i] > ar[i+1]){
+                int temp = ar[i];
+                ar[i] = ar[i-1];
+                ar[i-1] = temp;
+                swapped = 1;
             }
         }
     }
